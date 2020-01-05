1250124ec0ee2d5e54bb693e196357b9326370ec
Jeffery Myers <JeffM2501@gmail.com>
2020-01-05 14:24:27 -0800

    server with headless urho framework

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,12 +1,58 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+
+using Urho;
+using Urho.Desktop;
 
 namespace Server
 {
     class Program
     {
+        private static void Log(string text)
+        {
+            Console.WriteLine(text);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string dataPath = string.Empty; // TODO, parse this from optional command line
+
+            if (dataPath == string.Empty || !Directory.Exists(dataPath))
+                dataPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "assets");
+
+            if (!Directory.Exists(dataPath))
+            {
+                Log("Unable to locate data path " + dataPath);
+                return;
+            }
+
+            if (Directory.Exists(dataPath))
+            {
+                DesktopUrhoInitializer.AssetsDirectory = dataPath;
+
+                ApplicationOptions options = new ApplicationOptions("Data");
+                options.Orientation = ApplicationOptions.OrientationType.Landscape;
+                options.ResizableWindow = false;
+                options.WindowedMode = true;
+                options.AdditionalFlags = "-headless";
+
+
+                int exitCode = 0;
+                ServerApp app = null;
+                try
+                {
+                    app = new ServerApp(options);
+                    exitCode = app.Run();
+                }
+                catch (Exception ex)
+                {
+                    if (app == null || !app.IsExiting)
+                    {
+                        Log(ex.ToString());
+                    }
+                }
+            }
         }
     }
 }
--- Server/ServerApp.cs
+++ Server/ServerApp.cs
@@ -8,5 +8,24 @@ namespace Server
 {
     internal class ServerApp : Application
     {
+        public ServerApp(ApplicationOptions options = null) : base(options)
+        {
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+         
+            Update += App_Update;
+        }
+
+        private void App_Update(UpdateEventArgs obj)
+        {
+        }
+
+        protected override void OnUpdate(float timeStep)
+        {
+            base.OnUpdate(timeStep);
+        }
     }
 }
